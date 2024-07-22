2d2da11204e4cdeb24c29e6f9f743f6e1e113153
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2024-07-22 18:45:30 +0530

    𝗔𝗟𝗣𝗛𝗔

---
+++ RAUSHAN/database/__init__.py
@@ -0,0 +1,6 @@
+import motor.motor_asyncio
+
+from config import MONGO_URL
+cli = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
+
+dbb = cli.program
---
+++ RAUSHAN/database/gbandb.py
@@ -0,0 +1,34 @@
+from typing import Dict, List, Union
+
+from RAUSHAN.database import dbb as db
+
+gbansdb = db.gban
+
+
+async def gban_list() -> list:
+    users = gbansdb.find({"user_id": {"$gt": 0}})
+    users = await users.to_list(length=100000)
+    return users
+
+async def gban_count() -> int:
+    users = gbansdb.find({"user_id": {"$gt": 0}})
+    users = await users.to_list(length=100000)
+    return len(users)
+
+async def gban_info(user_id: int) -> bool:
+    user = await gbansdb.find_one({"user_id": user_id})
+    if not user:
+        return False
+    return True
+
+async def gban_user(user_id: int):
+    is_gbanned = await gban_info(user_id)
+    if is_gbanned:
+        return
+    return await gbansdb.insert_one({"user_id": user_id})
+
+async def ungban_user(user_id: int):
+    is_gbanned = await gban_info(user_id)
+    if not is_gbanned:
+        return
+    return await gbansdb.delete_one({"user_id": user_id})
