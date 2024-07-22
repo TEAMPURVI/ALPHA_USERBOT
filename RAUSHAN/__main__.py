e4a32750ba9199884f6ab561ff0e79d14bb418bd
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2024-07-22 18:44:13 +0530

    𝗔𝗟𝗣𝗛𝗔

---
+++ RAUSHAN/__main__.py
@@ -0,0 +1,26 @@
+import asyncio
+import importlib
+from pyrogram import Client, idle
+from RAUSHAN.helper import join
+from RAUSHAN.modules import ALL_MODULES
+from RAUSHAN import clients, app, ids
+
+async def start_bot():
+    await app.start()
+    print("LOG: Founded Bot token Booting Zeus.")
+    for all_module in ALL_MODULES:
+        importlib.import_module("RAUSHAN.modules" + all_module)
+        print(f"Successfully Imported {all_module} 💥")
+    for cli in clients:
+        try:
+            await cli.start()
+            ex = await cli.get_me()
+            await join(cli)
+            print(f"Started {ex.first_name} 🔥")
+            ids.append(ex.id)
+        except Exception as e:
+            print(f"{e}")
+    await idle()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
